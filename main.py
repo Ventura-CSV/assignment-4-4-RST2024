ccf815d2ef1e23a6753b51220b4d89b87267789f
RST2024 <rtanwani2024@gmail.com>
2024-06-28 13:01:54 -0700

    msg

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def main():
     Code Your Program here
     ########################################
     """
+    
+    for i in range (5):
+        num = int(input('Enter a number  '))
+        numbers.append(num)
+    minval = numbers[0]
+    maxval = numbers[0]
+    for j in numbers:
+        if j < minval:
+            minval = j
+        if j > maxval:
+            maxval = j
 
     print(*numbers)
     print(maxval, minval)
